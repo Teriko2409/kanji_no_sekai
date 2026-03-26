b129e95afeda81cc0d3891da3da7726390311076
Abraao Miguel Pereira Martins <abraao.pereira@etu.unige.ch>
2026-03-26 18:20:29 +0100

    Memokanji : Mise en place les cartes sur le plateau

--- dossier_godot/mini_jeu/MemoKanji/gameboard.gd
+++ dossier_godot/mini_jeu/MemoKanji/gameboard.gd
@@ -1,12 +1,23 @@
 extends Control
 
+const MODEL_CARTE = preload("res://mini_jeu/MemoKanji/cartes.tscn")
 
-# Called when the node enters the scene tree for the first time.
+@onready var grille = $GridContainer
+
+
+# Le jeu démarre ici !
 func _ready() -> void:
+	var mes_kanjis = []
+	
+	for kanji in mes_kanjis:
+		creer_une_carte(kanji)
 
-	pass # Replace with function body.
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+# Notre "machine" qui fabrique et range la carte
+func creer_une_carte(texte_du_kanji: String) -> void:
+	# a. On clone le modèle
+	var nouvelle_carte = MODEL_CARTE.instantiate()
+	
+	# b. On l'envoie dans la grille (le GridContainer la range tout seul !)
+	grille.add_child(nouvelle_carte)
+	
