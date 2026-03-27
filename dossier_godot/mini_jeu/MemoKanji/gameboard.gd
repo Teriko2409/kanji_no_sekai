646dfae89783d6c5adf7b13c2078ce86e7f5e93b
Abraao Miguel Pereira Martins <abraao.pereira@etu.unige.ch>
2026-03-27 11:10:20 +0100

    Memokanji : Correction de bug sur les carte + ajout du message sur les cartes

--- dossier_godot/mini_jeu/MemoKanji/gameboard.gd
+++ dossier_godot/mini_jeu/MemoKanji/gameboard.gd
@@ -5,19 +5,17 @@ const MODEL_CARTE = preload("res://mini_jeu/MemoKanji/cartes.tscn")
 @onready var grille = $GridContainer
 
 
-# Le jeu démarre ici !
 func _ready() -> void:
-	var mes_kanjis = []
+	var mes_kanjis = ["Salut ", "Laura"]
 	
 	for kanji in mes_kanjis:
 		creer_une_carte(kanji)
 
-
-# Notre "machine" qui fabrique et range la carte
 func creer_une_carte(texte_du_kanji: String) -> void:
-	# a. On clone le modèle
 	var nouvelle_carte = MODEL_CARTE.instantiate()
 	
-	# b. On l'envoie dans la grille (le GridContainer la range tout seul !)
 	grille.add_child(nouvelle_carte)
+	var mon_label = nouvelle_carte.get_node("kanji_wrote")
 	
+	if mon_label:
+		mon_label.text = texte_du_kanji
