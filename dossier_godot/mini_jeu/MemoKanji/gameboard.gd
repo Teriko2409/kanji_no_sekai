1a821db379dde667c06821736ccf35b7c3c6b443
Abraao Miguel Pereira Martins <abraao.pereira@etu.unige.ch>
2026-04-07 23:22:50 +0200

    Memokanji : shuffle + pair carte

--- dossier_godot/mini_jeu/MemoKanji/gameboard.gd
+++ dossier_godot/mini_jeu/MemoKanji/gameboard.gd
@@ -6,7 +6,6 @@ const json_url = preload("res://mini_jeu/MemoKanji/kanji_data_memokanji.json")
 
 @onready var structure = $structure_gameboard
 
-
 func _ready() -> void:
 	var kanji_list = downloaod_json_data()
 	
@@ -19,11 +18,27 @@ func create_cartes(text_parameter: String) -> void:
 	
 	structure.add_child(new_carte)
 	var label = new_carte.get_node("kanji_wrote")
+	label.text = text_parameter
 	
-	if label:
-		label.text = text_parameter
 		
-func downloaod_json_data () -> Array:
+func downloaod_json_data() -> Array:
+	var kanji_list = [
+		
+	"日", "月", "火", "水", "木", "金", "土",
+	"一", "二", "三", "四", "五", "六", "七", "八", "九", "十", 
+	"人", "子", "女", "男", 
+	"山", "川", "田", 
+	"大", "小", 
+	"上", "下", "左", "右", 
+	"口" 
+]
+
+	kanji_list.shuffle()
 	
-	var kanji_list = ["人", "間", "日", "月", "火", "水", "木", "金", "土", "本", "犬", "猫"]
+	kanji_list = kanji_list.slice(0, 6)
+	
+	kanji_list += kanji_list
+
+	kanji_list.shuffle()
+	
 	return kanji_list
